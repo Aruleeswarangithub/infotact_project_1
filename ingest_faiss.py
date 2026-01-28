486e13246b07fc146a4201e3d5d103a84885a0a3
Aruleeswarangithub <arulthirumalainathan@gmail.com>
2026-01-28 20:21:21 +0530

    Update ingest_faiss.py

--- ingest_faiss.py
+++ ingest_faiss.py
@@ -3,19 +3,20 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
-# Load PDF
+
 loader = PyPDFLoader("data/company_policy.pdf")
 documents = loader.load()
 
-# Chunking
+
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 chunks = splitter.split_documents(documents)
 
-# FREE Local Embeddings Model
+
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
-# Save FAISS DB
+
 db = FAISS.from_documents(chunks, embeddings)
 db.save_local("faiss_db")
 
 print("✅ FAISS DB created WITHOUT OpenAI")
+
